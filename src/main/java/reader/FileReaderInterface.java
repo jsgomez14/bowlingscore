683ece4aea82b892d78edbfd9f2e9b2142f0c9e8
Juan Sebastian Gómez Celis <js.gomez14@uniandes.edu.co>
2019-12-21 11:46:22 -0500

    Reader implementation

--- src/main/java/reader/FileReaderInterface.java
+++ src/main/java/reader/FileReaderInterface.java
@@ -4,5 +4,11 @@ import java.io.IOException;
 import java.util.List;
 
 public interface FileReaderInterface {
+    /**
+     * It reads a file given a path, and collect each line into a List
+     * @param path represents the path where the file is located
+     * @return List containing each line of the specified file.
+     * @throws IOException
+     */
     List<String> read(String path) throws IOException;
 }
